cb44094cbca7abfcb470a2747c54f455e8e05efd
Abigail Walthall <ghthor@gmail.com>
2013-03-24 20:05:44 -0400

    Implemented an equality function to compare the LottoGame struct

--- lottery_test.go
+++ lottery_test.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"testing"
 )
+
+func gamesAreEqual(a, b LottoGame) bool {
+	if a.Name != b.Name {
+		return false
+	}
+
+	if a.Cost != b.Cost {
+		return false
+	}
+
+	if len(a.Prizes) != len(b.Prizes) {
+		return false
+	}
+
+	for i := 0; i < len(a.Prizes); i++ {
+		if a.Prizes[i] != b.Prizes[i] {
+			return false
+		}
+	}
+
+	return true
+}
 
 func TestDataParser(t *testing.T) {
 	bytes, err := ioutil.ReadFile("scratcher.txt")
