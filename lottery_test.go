57e01192fd641bd1ab96c50f4aa6cdd6a0361532
Abigail Walthall <ghthor@gmail.com>
2013-03-24 20:06:19 -0400

    Compare parsed games with a set of expectedGames

--- lottery_test.go
+++ lottery_test.go
@@ -45,4 +45,60 @@ func TestDataParser(t *testing.T) {
 	if len(games) != 3 {
 		t.Error(errors.New("Invalid number of games parsed"))
 	}
+
+	expectedGames := []LottoGame{{
+		"SMALL BEANS",
+		1,
+		[]Prize{{
+			10000, 10, 10,
+		}, {
+			5000, 50, 8,
+		}, {
+			100, 100, 6,
+		}, {
+			10, 1000, 4,
+		}, {
+			1, 10000, 2,
+		}},
+	}, {
+		"PIRATE'S BOOTY, ARRR",
+		10,
+		[]Prize{{
+			50000, 20, 0,
+		}, {
+			10000, 100, 0,
+		}, {
+			1000, 500, 0,
+		}, {
+			100, 2000, 738,
+		}, {
+			10, 7500, 2945,
+		}, {
+			1, 10000, 4476,
+		}},
+	}, {
+		"BIG MONEY HU$TLA$",
+		20,
+		[]Prize{{
+			1000000, 10, 7,
+		}, {
+			500000, 50, 29,
+		}, {
+			10000, 100, 78,
+		}, {
+			1000, 500, 396,
+		}, {
+			100, 2000, 1439,
+		}, {
+			20, 5000, 3218,
+		}, {
+			10, 10000, 6210,
+		}},
+	}}
+
+	for i := 0; i < len(games); i++ {
+		if !gamesAreEqual(games[i], expectedGames[i]) {
+			t.Error(errors.New(fmt.Sprintf("     got: %v\nexpected: %v", games[i], expectedGames[i])))
+		}
+	}
 }
